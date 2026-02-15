b73120e8b244a52690595cd517c84cba823a5c10
BEMYDRE4M Development <KrynxtioNs@proton.me>
2026-02-15 17:19:40 +0100

    Add files via upload

---
+++ RoleplayBasicFramework/Client/Fraktionssystem/Duty.lua
@@ -0,0 +1,18 @@
+addEvent("Duty.window",true)
+addEventHandler("Duty.window",root,function()
+	if(isWindowOpen())then
+        GUIEditor.window[1] = guiCreateWindow(523, 241, 295, 112, "Dutysystem", false)
+
+        GUIEditor.button[1] = guiCreateButton(9, 25, 276, 33, "Dienst betreten", false, GUIEditor.window[1])
+        GUIEditor.button["Close"] = guiCreateButton(9, 68, 276, 33, "Schließen", false, GUIEditor.window[1])
+		if(getElementData(localPlayer,"duty") == true)then
+			guiSetText(GUIEditor.button[1],"Dienst verlassen")
+		end
+		
+		setWindowDatas("set")
+		
+		addEventHandler("onClientGUIClick",GUIEditor.button[1],function()
+			triggerServerEvent("Duty.server",localPlayer)
+		end,false)
+    end
+end)
